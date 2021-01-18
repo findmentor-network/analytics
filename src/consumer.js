c0545ddb7eb07c82502326c6cb8116935530ed6d
Berkay Beyaz <berkaybeyaz@bugece.co>
2021-01-18 04:12:09 +0300

    WIP: Added rabbitmq connection

---
+++ src/consumer.js
@@ -0,0 +1,14 @@
+const broker = require('./broker');
+const db = require('./db');
+
+Promise.all([
+  broker.connect(),
+  db.connect(),
+])
+  .then(() => {
+    broker.consume((data) => {
+      // Business Logic
+      console.log('Received new message', data.event);
+      db.add(data);
+    });
+  });
